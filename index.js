5e60d388b2c3799d9a4cc26c36394f7af0af451b
Super <superwebtop@outlook.com>
2019-11-27 10:54:10 -0800

    async

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const catchError = (callback) => {
 }
 
   
-const srapping = (req, res) => {
+const srapping = async(req, res) => {
   const {url} = req.body
   const hostname = urlParser.parse(url).protocol + '//' + urlParser.parse(url).hostname;
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
